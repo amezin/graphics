bbb8eecc92634938bf8477aae814439026ba5659
Alexander Mezin <mezin.alexander@gmail.com>
2012-05-18 09:33:11 +0700

    Build trefoll knot

--- Knot/src/FIT_9202_Mezin/Knot/HermiteKnotBuilder.java
+++ Knot/src/FIT_9202_Mezin/Knot/HermiteKnotBuilder.java
@@ -2,13 +2,37 @@ package FIT_9202_Mezin.Knot;
 
 public class HermiteKnotBuilder extends KnotBuilder {
 
+	private static final double R = 100.0;
+	private static final int POINTS = 6;
+
 	public static Knot build() {
 		HermiteKnotBuilder builder = new HermiteKnotBuilder();
 
-		double r = KnotSettings.R_DEFAULT * 0.5;
+		double t = 0.0;
+		double step = Math.PI * 2.0 / POINTS;
+		for (int i = 0; i < POINTS + 1; i++, t += step) {
+			double y = -(2.0 + Math.cos(3.0 * t)) * Math.cos(2.0 * t) * R;
+			double x = (2.0 + Math.cos(3.0 * t)) * Math.sin(2.0 * t) * R;
+			double z = Math.sin(3.0 * t) * R;
 
-		builder.addSegment(-r, 0, 0, 0, 0, -r * 2);
-		builder.addSegment(r, 0, 0, 0, 0, r * 2);
+			double ny = -(2.0 + Math.cos(3.0 * (t + step * 0.5)))
+					* Math.cos(2.0 * (t + step * 0.5)) * R;
+			double nx = (2.0 + Math.cos(3.0 * (t + step * 0.5)))
+					* Math.sin(2.0 * (t + step * 0.5)) * R;
+			double nz = Math.sin(3.0 * (t + step * 0.5)) * R;
+
+			double py = -(2.0 + Math.cos(3.0 * (t - step * 0.5)))
+					* Math.cos(2.0 * (t - step * 0.5)) * R;
+			double px = (2.0 + Math.cos(3.0 * (t - step * 0.5)))
+					* Math.sin(2.0 * (t - step * 0.5)) * R;
+			double pz = Math.sin(3.0 * (t - step * 0.5)) * R;
+
+			double dx = x - px + (nx - x);
+			double dy = y - py + (ny - y);
+			double dz = z - pz + (nz - z);
+
+			builder.addSegment(x, y, z, dx, dy, dz);
+		}
 
 		return builder.buildKnot();
 	}
